efaf60694b9ff382d634863c39e03e1f6008b247
kkatayama <kkatayama@udel.edu>
2023-01-11 16:41:01 -0500

    added support for query with multiple terms

--- setup.py
+++ setup.py
@@ -7,18 +7,18 @@ Usage: brewls [keyword]
 
 from setuptools import setup
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     required = f.read().splitlines()
 
-setup(name='brewls',
-      version='1.0.2',
-      description='A better homebrew package search',
-      url='http://github.com/kkatayama/brewls',
-      author='Teddy',
-      author_email='katayama@udel.edu',
-      license='MIT',
-      packages=['brewls'],
-      python_requires='>=3',
-      scripts=['bin/brewls'],
+setup(name="brewls",
+      version="1.0.3",
+      description="A better homebrew package search",
+      url="http://github.com/kkatayama/brewls",
+      author="Teddy",
+      author_email="katayama@udel.edu",
+      license="MIT",
+      packages=["brewls"],
+      python_requires=">=3",
+      scripts=["bin/brewls"],
       install_requires=required,
       zip_safe=False)
